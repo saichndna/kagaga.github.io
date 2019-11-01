ffa35cddea08ab81dc44058c4eda16c7d1b0ade3
Kyle Zheng <kylezheng73@gmail.com>
2019-11-01 00:04:18 -0400

    logo stays centered

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,58 +1,50 @@
 
-var TxtType = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
+var TextWriter = function (element, wordsList, period) {
+  this.wordsList = wordsList;
+  this.element = element;
   this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
+  this.period = period;
   this.txt = '';
   this.tick();
   this.isDeleting = false;
 };
 
-TxtType.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
+TextWriter.prototype.tick = function () {
+  // var i = this.loopNum % this.wordsList.length;
+  var fullTxt = this.wordsList[this.loopNum];
 
   if (this.isDeleting) {
-  this.txt = fullTxt.substring(0, this.txt.length - 1);
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
   } else {
-  this.txt = fullTxt.substring(0, this.txt.length + 1);
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
 
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  this.element.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
   var that = this;
   var delta = 200 - Math.random() * 100;
-
   if (this.isDeleting) { delta /= 2; }
 
   if (!this.isDeleting && this.txt === fullTxt) {
-  delta = this.period;
-  this.isDeleting = true;
+    delta = this.period;
+    this.isDeleting = true;
   } else if (this.isDeleting && this.txt === '') {
-  this.isDeleting = false;
-  this.loopNum++;
-  delta = 500;
+    this.isDeleting = false;
+    this.loopNum++;
+    if (this.loopNum == this.wordsList.length) {
+      this.loopNum = 0;
+    }
+    delta = 500;
   }
 
-  setTimeout(function() {
-  that.tick();
+  setTimeout(function () {
+    that.tick();
   }, delta);
 };
 
-window.onload = function() {
-  var elements = document.getElementsByClassName('typewrite');
-  for (var i=0; i<elements.length; i++) {
-      var toRotate = elements[i].getAttribute('data-type');
-      var period = elements[i].getAttribute('data-period');
-      if (toRotate) {
-        new TxtType(elements[i], JSON.parse(toRotate), period);
-      }
-  }
-  // INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-  document.body.appendChild(css);
-};
+let changingNoun = document.getElementById("changing-noun")
+const nouns = ["fraternity", "sorority", "crack house", "middle lounge", "gambling ring", "home"]
+
+new TextWriter(changingNoun, nouns, 2000)
+
 
